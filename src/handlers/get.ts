daecc0453964ebcfd150e2b6fa07aa343e351708
anthonyDrury <anthony.drury1@gmail.com>
2020-10-30 07:06:08 +1100

    feat: remove excess code an debug statements

--- src/handlers/get.ts
+++ src/handlers/get.ts
@@ -5,6 +5,5 @@ import { getNode } from "../clients/dynamo-tree.client";
 
 export const getNodeHandler: APIGatewayProxyHandler = async (event) => {
   const nodeId = event.queryStringParameters.nodeId;
-  console.log(nodeId);
   return await getNode(nodeId);
 };
